a60ccafafffd2fd06eea528c58a7d8307c8d46fd
Corvettus <mackar.danilov@yandex.ru>
2022-05-04 01:15:50 +0300

    136 * удален воркспейс xml

---
+++ src/main/java/solution136/Solution.java
@@ -0,0 +1,13 @@
+package solution136;
+
+class Solution {
+    public int singleNumber(int[] nums) {
+        int result = 0;
+
+        for (int n : nums) {
+            result ^= n;
+        }
+
+        return result;
+    }
+}
