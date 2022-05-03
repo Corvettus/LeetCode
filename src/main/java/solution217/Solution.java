5ce9d42a27f269f92e9b208dbe38da42757b9e81
Corvettus <mackar.danilov@yandex.ru>
2022-05-03 16:19:18 +0300

    solution217 * 217 solved

---
+++ src/main/java/solution217/Solution.java
@@ -0,0 +1,19 @@
+package solution217;
+
+import java.util.HashSet;
+import java.util.Set;
+
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        Set<Integer> metNumbers = new HashSet<>();
+
+        for (int n : nums) {
+            if (metNumbers.contains(n)) {
+                return true;
+            }
+            metNumbers.add(n);
+        }
+
+        return false;
+    }
+}
