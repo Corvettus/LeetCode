d4cf15653f0ed853167743f06ae5440ec78ef9cf
Corvettus <mackar.danilov@yandex.ru>
2022-07-19 23:02:51 +0300

    1 solved with HashMap

--- src/main/java/easy/problem_1/Solution.java
+++ src/main/java/easy/problem_1/Solution.java
@@ -1,15 +1,24 @@
 package easy.problem_1;
 
+import java.util.HashMap;
+
 public class Solution {
     public int[] twoSum(int[] nums, int target) {
-        for (int i = 0; i < nums.length; i++) {
-            for (int j = i; j < nums.length; j++) {
-                if (nums[i] + nums[j] == target) {
-                    return new int[]{i, j};
-                }
-            }
+        if (nums == null || nums.length < 2) {
+            return null;
         }
 
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            int diff = target - nums[i];
+
+            if (!map.containsKey(diff)) {
+                map.put(nums[i], i);
+            } else {
+                return new int[]{map.get(diff), i};
+            }
+        }
         return null;
     }
 }
